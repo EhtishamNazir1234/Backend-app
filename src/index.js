a6870eb78f9535ded55d08b08ecb2958e0054e06
ehtishamnazir1234@gmail.com <ehtishamnazir1234@mail.com>
2025-05-12 17:43:46 +0500

    Connected to MongoDB atlas

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+
+import connectDB from "./db/db.js";
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB();
+
+/*
+import express from "express";
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+    app.on("error",(error)=>{
+        console.log( "Error connecting to MongoDB:",error);
+        throw error;
+        
+    })
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+        
+    })
+  } catch (error) {
+    console.error("ERROR:", error);
+    throw error;
+  }
+})();
+*/
